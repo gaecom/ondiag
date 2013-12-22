2d967f8c7361a60cf923a7233b847bce24b251d7
fabiodoctorsys <fabiocariati@gmail.com>
2013-12-22 12:09:32 -0200

    Rails structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ondiag::Application.config.secret_key_base = '4329659ba9bb55f4ff60520fbc6ff668432cc64c0007ce09009beb08d84eb75cced3c6355ecbe59c7f9dd3bbf7368e828f74c7f2fc5ab90e6c96083512059d2e'
